ac8865b7a25f2af49d12e568a1dd42ae0c79e932
steveming0409 <s9t1e1v7e5@gmail.com>
2018-12-20 15:07:48 +0800

    lab12

---
+++ app/src/main/java/com/example/user/myapplication/Data.java
@@ -0,0 +1,14 @@
+package com.example.user.myapplication;
+
+public class Data {
+    Result result;
+
+    class Result{
+        Results[] results;
+
+        class Results{
+            String Station;
+            String Destination;
+        }
+    }
+}
